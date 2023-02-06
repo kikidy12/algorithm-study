959335565a118e2441611d42084b78f9af2c7e03
kikidy12 <kikidy12@naver.com>
2023-02-06 13:12:54 +0900

    둘만의 암호 수정

--- src/programers/단계1/둘만의암호/Main.java
+++ src/programers/단계1/둘만의암호/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("aukks", "wbqd", 5));;
+        System.out.println(solution.solution("aukks", "wbqd", 5));
     }
 }
 
