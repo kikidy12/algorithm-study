d3e0f42146db24ffb287b87b0923aef674a123d4
kikidy12 <kikidy12@naver.com>
2023-02-06 17:58:00 +0900

    프로그래머스 - 가장 가까운 글자, 과일장수, 기사단원의 무기, 명예의 전당(1), 문자열 나누기, 옹알이(2), 크기가 작은 부분문자열, 햄버거 만들기

---
+++ src/programers/단계1/옹알이2/Main.java
@@ -0,0 +1,58 @@
+package programers.단계1.옹알이2;
+
+//문제 설명
+//
+//머쓱이는 태어난 지 11개월 된 조카를 돌보고 있습니다.
+// 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고
+// 연속해서 같은 발음을 하는 것을 어려워합니다. 문자열 배열 babbling이 매개변수로 주어질 때,
+// 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+//제한사항
+//1 ≤ babbling의 길이 ≤ 100
+//1 ≤ babbling[i]의 길이 ≤ 30
+//문자열은 알파벳 소문자로만 이루어져 있습니다.
+//입출력 예
+//babbling	result
+//["aya", "yee", "u", "maa"]	1
+//["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]	2
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"}));
+    }
+}
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        String[] arr = {"aya", "ye", "woo", "ma"};
+
+        String[] arr2 = {"ayaaya", "yeye", "woowoo", "mama"};
+
+        first : for (String s : babbling) {
+            for (String s2 : arr2) {
+                if (s.contains(s2)) {
+                    continue first;
+                }
+            }
+            for (String s1 : arr) {
+                if (s.contains(s1)) {
+                    s = s.replaceAll(s1, "1");
+                }
+            }
+
+            s = s.replaceAll("1", "");
+
+            if (s.isEmpty()) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
